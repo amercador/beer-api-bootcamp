8c124727a79dd9e3608c77f4fc632a23ca420f6a
amercador <amercador@hotmail.com>
2021-07-20 23:41:13 -0300

    Criada as classes de Testes

---
+++ src/test/java/com/digitalinnovationone/beerapi/utils/BeerstockApplicationTests.java
@@ -0,0 +1,14 @@
+package com.digitalinnovationone.beerapi.utils;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
